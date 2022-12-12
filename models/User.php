c357cfa83054145d51b4548b148441ebe9b69cab
riamohan0106 <60840140+riamohan0106@users.noreply.github.com>
2022-12-12 10:26:47 +0530

    Add files via upload

---
+++ models/User.php
@@ -0,0 +1,82 @@
+<?php
+require_once '../DB-Config/Database.php';
+//include  '../controllers/Users.php';
+
+class User {
+
+    private $db;
+
+    public function __construct(){
+        $this->db = new Database;
+    }
+
+    //Find user by email or mobile
+    public function findUserByEmailOrMobile($email, $usermobile){
+        $this->db->query('SELECT * FROM users WHERE usersMobile = :usermobile OR usersEmail = :email');
+//        binding the values from the returned query according to our needs.
+        $this->db->bind(':usermobile', $usermobile);
+        $this->db->bind(':email', $email);
+//      The single method will execute the query in a way that ensures there is only a single corresponding value.
+//      The func finduserbyemailorusername is used for validating check.
+        $row = $this->db->single();
+
+        //Check row
+        if($this->db->rowCount() > 0){
+            return $row;
+        }else{
+            return false;
+        }
+    }
+
+    //Register User
+    public function register($data){
+        $this->db->query('INSERT INTO users (usersFName, usersLName, usersGender,usersDob,usersMobile,usersEmail, usersPwd) 
+        VALUES (:firstname, :lastname, :gender, :DOB, :mobile, :image, :email, :password)');
+        //Bind values
+        $this->db->bind(':firstname', $data['usersFName']);
+        $this->db->bind(':lastname', $data['usersLName']);
+        $this->db->bind(':gender', $data['usersGender']);
+        $this->db->bind(':DOB', $data['usersDob']);
+        $this->db->bind(':mobile', $data['usersMobile']);
+//        $this->db->bind(':image',$filename);
+        $this->db->bind(':email', $data['usersEmail']);
+        $this->db->bind(':password', $data['usersPwd']);
+//        move_uploaded_file($image_tmp_name,$image_folder);
+
+
+        //Execute
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    //Login user
+    public function login($mobileOrEmail, $password){
+        $row = $this->findUserByEmailOrMobile($mobileOrEmail, $mobileOrEmail);
+
+        if($row == false) return false;
+
+        $hashedPassword = $row->usersPwd;
+        if(password_verify($password, $hashedPassword)){
+            return $row;
+        }else{
+            return false;
+        }
+    }
+
+    //Reset Password
+    public function resetPassword($newPwdHash, $tokenEmail){
+        $this->db->query('UPDATE users SET usersPwd=:pwd WHERE usersEmail=:email');
+        $this->db->bind(':pwd', $newPwdHash);
+        $this->db->bind(':email', $tokenEmail);
+
+        //Execute
+        if($this->db->execute()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
